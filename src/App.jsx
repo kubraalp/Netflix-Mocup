fac883f8e7fd739a9d5b48c792abf205d2c96876
Kubra Alp <kubra.alp.k@gmail.com>
2024-06-13 15:36:24 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { Link, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import "./App.css";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import Home from "./pages/Home";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Kübra</h1>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <h1>Welcome to Netflix by Wit</h1>
+        </Route>
+        <Route path="login">
+          <Header />
+          <Login />
+        </Route>
+        <Route path="Welcome">
+          <Welcome />
+        </Route>
+        <Route path="home">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
+      <div className="dev-navigation">
+        <Link to="/">HomePage</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/home">Home</Link>
+      </div>
+    </>
+  );
 }
 
 export default App;
