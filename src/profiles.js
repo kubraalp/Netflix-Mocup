a602c212dbb52a42f490d35a3301481abd837576
Kubra Alp <kubra.alp.k@gmail.com>
2024-06-20 21:39:14 +0300

    dymmy profiles data added for development

---
+++ src/profiles.js
@@ -0,0 +1,27 @@
+export const profiles = [
+  {
+    id: 7,
+    name: "Michael",
+    avatar: "https://reqres.in/img/faces/7-image.jpg",
+  },
+  {
+    id: 8,
+    name: "Lindsay",
+    avatar: "https://reqres.in/img/faces/8-image.jpg",
+  },
+  {
+    id: 9,
+    name: "Tobias",
+    avatar: "https://reqres.in/img/faces/9-image.jpg",
+  },
+  {
+    id: 10,
+    name: "Byron",
+    avatar: "https://reqres.in/img/faces/10-image.jpg",
+  },
+  {
+    id: 11,
+    name: "George",
+    avatar: "https://reqres.in/img/faces/11-image.jpg",
+  },
+];
